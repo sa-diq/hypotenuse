866e5bb7f919fc7ea6b1f5a8a18e642f1347988e
Sadiq Balogun, MSc MBCS <122885431+sa-diq@users.noreply.github.com>
2024-10-14 10:36:17 +0000

    Adding files

---
+++ src/hypotenuse/pythagos.py
@@ -0,0 +1,32 @@
+# Module to calculate the hypotenuse of a right angled triangle
+
+# Function to add two numbers
+# input: float or int or list/array of ints/floats
+def add_nums(a,b):
+    """Add two numbers together
+
+    Args:
+        a (int, float, array): A number to add
+        b (int, float, array): A number to add
+
+    Returns:
+        _type_: _description_
+    """
+    sum_of_nums = a + b
+    return sum_of_nums
+    
+
+# Function to calculate the square of a number
+# input: float or int or list/array of ints/floats
+
+
+
+# Function to calculate the square root of a number
+# input: float or int or list/array of ints/floats
+
+
+
+# Function to calculate hypotenuse using all of the above
+# formula: a^2 = b^2 + c^2
+# c = sqrt(a^2 +b^2)
+# input: float or int or list/array of ints/floats
